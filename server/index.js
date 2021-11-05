6d9c6fe0fe41132d02982ccbe53b734e1a05bef8
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-05 18:26:16 -0500

    duplicated compliment button data and manipulated it to be fortunes

--- server/index.js
+++ server/index.js
@@ -22,4 +22,26 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+
+///////////////////////
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes =[" A smooth long journey! Great expectations.",
+  "Curiosity kills boredom. Nothing can kill curiosity." ,
+  "Education is the ability to meet life’s situations.",
+  "Good to begin well, better to end well.",
+  "Imagination rules the world.",
+
+  ];
+
+ 
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+
+});
+  
+
+
 app.listen(4000, () => console.log("Server running on 4000"));
